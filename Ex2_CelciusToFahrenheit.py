a22a5c33ae4f851d3dde91c24e80e8a0059e0f52
GogoShara <157387153+GogoShara@users.noreply.github.com>
2024-05-27 23:39:06 -0300

    Create Ex2_CelciusToFahrenheit.py

---
+++ Ex2_CelciusToFahrenheit.py
@@ -0,0 +1,5 @@
+print ("SUPER CONVERSOR DE CELCIUS PARA FAHRENHEIT ONLINE GRÁTIS")
+Cel= int(input("Digite o valor em Celcius a ser convertido: "))
+Fah=Cel*(9/5)+32
+print ("Seu valor em Celcius de " + str(Cel) + " graus"+ " em Fahrenheit é " + str(Fah))
+print ("clique 'Enter' para fechar seu programa")
