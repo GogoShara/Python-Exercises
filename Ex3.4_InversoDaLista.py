cf2341c0a29026442e8178daf46264588122876f
GogoShara <157387153+GogoShara@users.noreply.github.com>
2024-06-04 09:03:33 -0300

    Reversing a List in Python Exercise

---
+++ Ex3.4_InversoDaLista.py
@@ -0,0 +1,10 @@
+lista= [4,9,0,5,6,16]
+esq= 0
+dire= len(lista)-1
+while (esq < dire):
+    ihi = lista[esq]
+    lista[esq] = lista[dire]
+    lista[dire] = ihi
+    esq = esq + 1
+    dire = dire - 1
+print (lista)
