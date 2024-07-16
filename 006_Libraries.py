2e30d0d81636a116512fe6ca05e3c5670fbd9193
GogoShara <157387153+GogoShara@users.noreply.github.com>
2024-07-15 21:57:39 -0300

    Time+Random+OS
    I display a message with a radom time delay. Also a color is displayed on OS

---
+++ 006_Libraries.py
@@ -0,0 +1,12 @@
+import time
+import random
+import os
+
+print("Oi Val")
+time.sleep(1)
+print("Tudo bem?")
+time.sleep(random.randrange(0, 4))
+os.system("color 5a")
+os.system("cls")
+time.sleep(1)
+print("Agora ta tudo bem")
