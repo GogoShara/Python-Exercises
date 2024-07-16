475dc46712b650672302a5540d041e66b6803ce9
GogoShara <157387153+GogoShara@users.noreply.github.com>
2024-07-15 21:51:40 -0300

    Bigger or Smaller than 10
    You type a number, and the program will tell you if it's bigger or smaller than 10.

---
+++ 001.py
@@ -0,0 +1,11 @@
+print("Bem vindo ao programa!! >____<")
+a= int(input("Digite seu número: "))
+
+
+if(a > 10):
+    print("É maior")
+elif(a == 10):
+    print("É igual a 10")
+else:
+    print("Nao é maior que 10")
+    
